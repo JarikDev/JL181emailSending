2e8f4085bacfcf9a07400bcfa67e7af797278b7b
ivan4000 <sxln1@mail.ru>
2018-11-13 23:50:58 +0300

    JL181emailSending

--- src/com/Main.java
+++ src/com/Main.java
@@ -1,44 +1,48 @@
 package com;
 
-
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.Transport;
-import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import javax.swing.*;
+import java.awt.*;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
-
 public class Main {
-    public static void main(String[] args) throws IOException, MessagingException {
-        final Properties properties = new Properties();
-        properties.load(new FileInputStream("src/mail.properties"));
+    static JFrame jFrame = getFrame();
+    static JPanel jPanel = new JPanel();
 
-        Session mailSession = Session.getDefaultInstance(properties);
-        MimeMessage message = new MimeMessage(mailSession);
-        message.setFrom(new InternetAddress("***@gmail.com"));
-        message.addRecipient(Message.RecipientType.TO, new InternetAddress("sxln1@mail.ru"));
-        message.setSubject("Hello email !!!");
-        message.setText("HI! this is my test message");
+    public static void main(String[] args) {
+        jFrame.add(jPanel);
+        GridBagLayout gridBagLayout = new GridBagLayout();
+        jPanel.setLayout(gridBagLayout);
 
-        for (int i = 0; i < 3; i++) {
-            Transport transport=mailSession.getTransport();
-            transport.connect(null,"***");
-            transport.sendMessage(message,message.getAllRecipients());
-            transport.close();
-        }
+        GridBagConstraints cont1 = new GridBagConstraints();
+        cont1.weightx = 0;
+        cont1.weighty = 0;
+        cont1.gridx = 0;
+        cont1.gridy = 0;
+        cont1.gridheight = 2;
+        cont1.gridwidth = 2;
+        jPanel.add(new JTextField("Title", 20), cont1) ;
 
+        jPanel.revalidate();
+    }
+    static JFrame getFrame() {
+        JFrame jFrame = new JFrame() {};
+        jFrame.setVisible(true);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension dimension = toolkit.getScreenSize();
+        jFrame.setBounds(dimension.width / 2 - 700, dimension.height / 2 - 300, 700, 700);
+        return jFrame;
 
     }
 }
-
-
-
-
 
 
 
